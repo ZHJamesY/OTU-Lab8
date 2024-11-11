d1252022fceaa58ef0825cec8609e57bc0da8d8e
JamesZHY <1195771246y@gmail.com>
2024-11-11 11:08:13 -0500

    lint - fixed all styling issues

--- quiz_app/src/models/quiz_model.py
+++ quiz_app/src/models/quiz_model.py
@@ -1,13 +1,14 @@
 # src/models/quiz_model.py
 from src.database import db
 
+
 # INCOMPLETE: Define the QuizModel class
 # TODO: Create a class named QuizModel that inherits from db.Model
 class QuizModel(db.Model):
     # INCOMPLETE: Set up the table name
     # TODO: Define `__tablename__` as 'quizzes'
-    __tablename__ = 'quizzes'
-    
+    __tablename__ = "quizzes"
+
     # INCOMPLETE: Define table columns
     # TODO: Define an integer primary key column named `id`
     # TODO: Define a string column named `title` that cannot be null
@@ -15,16 +16,18 @@ class QuizModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     questions = db.Column(db.PickleType)
-    
+
     def __init__(self, title, questions):
         # INCOMPLETE: Initialize the model with title and questions
-        # TODO: Assign `self.title` and `self.questions` with `title` and `questions`
+        # TODO: Assign `self.title` and `self.questions` with `title` and
+        # `questions`
         self.title = title
         self.questions = questions
 
     def save(self):
         # INCOMPLETE: Save the quiz to the database
-        # TODO: Use `db.session.add(self)` and `db.session.commit()` to save the instance
+        # TODO: Use `db.session.add(self)` and `db.session.commit()` to save
+        # the instance
         db.session.add(self)
         db.session.commit()
 
