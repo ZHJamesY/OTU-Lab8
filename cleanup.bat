e4c722a41591217d1930df0e62a6948d48661c19
JamesZHY <1195771246y@gmail.com>
2024-11-11 13:33:59 -0500

    updated cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -4,5 +4,9 @@ echo Cleaning up Python cache files...
 :: Remove all __pycache__ directories
 FOR /d /r %%d IN ("__pycache__") DO @IF EXIST "%%d" rd /s /q "%%d"
 
+:: Remove all .pytest_cache directories
+FOR /d /r %%d IN (".pytest_cache") DO @IF EXIST "%%d" rd /s /q "%%d"
+
+
 echo Cleanup complete!
 pause
